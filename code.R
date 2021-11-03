63185ff6a84c5a1ad2141b9eb37d92946bca9774
sanghyuk <sanghyuk.bae@gmail.com>
2021-11-03 14:40:31 +0900

    11월 4일

--- code.R
+++ code.R
@@ -237,3 +237,248 @@ with(busan_short, {
   lines(sort(pm10_s1), predict(f, data.frame(pm10_s1 = sort(pm10_s1))),lwd = 1)
 })
 
+#6. 관련성 분석
+# PM is weak signal
+library(HEAT)
+seoul<-lagdata(seoul,c("pm10","meantemp","o3"),1)
+fit <- glm(all_tot ~ pm10_s1 + ns(date, 4 * 14) + ns(meantemp, 6) + dow, data = seoul, na.action = na.exclude, family = poisson)
+pr<-predict(fit, type="terms")
+pred<-as.data.frame(pr)
+names(pred)<-c("pred_pm","pred_date","pred_temp","pred_dow")
+pred$date<-as.Date(seoul$date)
+
+with(pred,plot(date,pred_date, type="l",lty=2,xlab="",ylab=""))
+with(pred,lines(date,pred_temp,lwd=1, col="grey"))
+with(pred,lines(date,pred_pm,lwd=1))
+
+with(pred,plot(date,pred_pm,type="l"))
+
+#single lag models in Seoul
+library(tsModel)
+f0 <- glm(all_tot ~ pm10 + meantemp, data = seoul, family = poisson)
+f1 <- glm(all_tot ~ Lag(pm10, 1) + meantemp, data = seoul, family = poisson)
+f2 <- glm(all_tot ~ Lag(pm10, 2) + meantemp, data = seoul, family = poisson)
+f3 <- glm(all_tot ~ Lag(pm10, 3) + meantemp, data = seoul, family = poisson)
+f4 <- glm(all_tot ~ Lag(pm10, 4) + meantemp, data = seoul, family = poisson)
+ss <- list(summary(f0), summary(f1), summary(f2),summary(f3), summary(f4))
+models <- lapply(ss, function(x) x$coefficients[2,c("Estimate", "Std. Error")])
+models
+
+#distributed lag model
+fit <- glm(all_tot ~ Lag(pm10, 0:4) + meantemp, data = seoul, family = poisson)
+summ <- summary(fit)
+summ
+
+rn <- rownames(summ$coefficients)
+i <- grep("pm10", rn, fixed = TRUE)
+coefs <- summ$coefficients[i, "Estimate"]
+total <- sum(coefs)
+total
+
+#부산 temperature lags
+busan<-lagdata(busan,"pm10",3)
+busan.sub<-subset(busan,year<2011)
+library(tsModel)
+maxlag <- 0:13
+models <- sapply(maxlag, function(mlag) {
+  fit <- glm(all_tot ~ pm10_s1 + Lag(meantemp, seq(0, mlag)), data = busan.sub,family = poisson)
+  summ <- summary(fit)
+  summ.coef <- summ$coefficients["pm10_s1",2]
+  c(coef(fit)["pm10_s1"], summ.coef)
+})
+
+rng <- range(models[1, ] - 1.96 * models[2,], models[1, ] + 1.96 * models[2, ], 0)
+par(mar = c(4, 5, 1, 1))
+plot(maxlag, models[1, ], type = "b",pch = 20, ylim = rng, xlab = "Maximum temperature lag",ylab = expression(hat(beta) * " for " *
+                                                                                                                PM[10] * " at lag 1"))
+lines(maxlag, models[1, ] + 1.96 * models[2,], lty = 2)
+lines(maxlag, models[1, ] - 1.96 * models[2, ], lty = 2)
+abline(h = 0, lty = 3)
+
+#부산 warm season
+busan.warm <- subset(busan.sub, quarters(date) %in% c("Q2", "Q3"))
+models.warm <- sapply(maxlag, function(mlag) {
+  fit <- glm(all_tot ~ pm10_s1 + Lag(meantemp, seq(0, mlag)), data = busan.warm,family = poisson)
+  summ <- summary(fit)
+  summ.coef <- summ$coefficients["pm10_s1",2]
+  c(coef(fit)["pm10_s1"], summ.coef)
+})
+
+#부산 cold season
+busan.cold <- subset(busan.sub, quarters(date) %in% c("Q1", "Q4"))
+models.cold <- sapply(maxlag, function(mlag) {
+  fit <- glm(all_tot ~ pm10_s1 + Lag(meantemp, seq(0, mlag)), data = busan.cold,family = poisson)
+  summ <- summary(fit)
+  summ.coef <- summ$coefficients["pm10_s1",2]
+  c(coef(fit)["pm10_s1"], summ.coef)
+})
+
+#그림
+library(lattice)
+trellis.par.set(theme = canonical.theme("pdf",FALSE))
+y <- c(models.warm[1, ], models.cold[1,])
+xpts <- rep(maxlag, 2)
+f <- gl(2, length(maxlag), labels = c("Warm season","Cold season"))
+std <- c(models.warm[2, ], models.cold[2,])
+rng <- range(y - 1.96 * std, y + 1.96 *std, 0)
+rng <- rng + c(-1, 1) * 0.05 * diff(rng)
+ylab <- expression(hat(beta) * " for " *PM[10] * " at lag 1")
+p <- xyplot(y ~ xpts | f, as.table = TRUE, ylim = rng, subscripts = TRUE, panel = function(x,y, subscripts, ...) {
+  panel.xyplot(x, y, ...)
+  llines(x, y - 1.96 * std[subscripts],lty = 2)
+  llines(x, y + 1.96 * std[subscripts],lty = 2)
+  panel.abline(h = 0, lty = 3)
+}, xlab = "Maximum temperature lag",
+layout = c(1, 2), type = "b", ylab = ylab,
+pch = 20)
+print(p)
+
+#time smooth function 보정
+models <- sapply(maxlag, function(mlag) {
+  fit <- glm(all_tot ~ pm10_s1 + Lag(meantemp, seq(0, mlag))+ns(date,4*5), data = busan.sub,family = poisson)
+  summ <- summary(fit)
+  summ.coef <- summ$coefficients["pm10_s1",2]
+  c(coef(fit)["pm10_s1"], summ.coef)
+})
+rng <- range(models[1, ] - 1.96 * models[2,], models[1, ] + 1.96 * models[2, ], 0)
+par(mar = c(4, 5, 1, 1))
+plot(maxlag, models[1, ], type = "b",pch = 20, ylim = rng, xlab = "Maximum temperature lag",ylab = expression(hat(beta) * " for " *
+                                                                                                                PM[10] * " at lag 1"))
+lines(maxlag, models[1, ] + 1.96 * models[2,], lty = 2)
+lines(maxlag, models[1, ] - 1.96 * models[2, ], lty = 2)
+abline(h = 0, lty = 3)
+
+#smooth of mortality
+library(gam)
+library(splines)
+xpts <- seq(as.Date("2006-01-01"), as.Date("2018-12-31"), "day")
+fit2 <- gam(all_tot ~ s(date, 2 * 13), family = poisson, data = busan)
+summary(fit2)
+p2 <- predict(fit2, data.frame(date = xpts),type = "response")
+fit12 <- gam(all_tot ~ s(date, 12 * 13),family = poisson, data = busan)
+p12 <- predict(fit12, data.frame(date = xpts),type = "response")
+plot(xpts,busan$all_tot, pch=46, ylab="Mortality count", xlab="")
+lines(xpts,p2)
+lines(xpts,p12, col="gray")
+
+#smooths of pm10 and o3
+par(mfrow=c(1,2))
+fit2pm10 <- gam(pm10 ~ s(date, 2 * 13), data = busan)
+p2pm10 <- predict(fit2pm10, data.frame(date = xpts),type = "response")
+fit12pm10 <- gam(pm10 ~ s(date, 12 * 13), data = busan)
+p12pm10 <- predict(fit12pm10, data.frame(date = xpts),type = "response")
+plot(xpts,busan$pm10, pch=46, ylab="PM10", xlab="", ylim=c(0,150))
+lines(xpts,p2pm10)
+lines(xpts,p12pm10, col="gray")
+
+fit2o3 <- gam(o3 ~ s(date, 2 * 13),  data = busan)
+p2o3 <- predict(fit2o3, data.frame(date = xpts),type = "response")
+fit12o3 <- gam(o3 ~ s(date, 12 * 13), data = busan)
+p12o3 <- predict(fit12o3, data.frame(date = xpts),type = "response")
+plot(xpts,busan$o3, pch=46, ylab="O3", xlab="")
+lines(xpts,p2o3)
+lines(xpts,p12o3, col="gray")
+
+#different dfs
+par(mfrow=c(1,2))
+dfValues <- c(2, 4, 6, 8, 10, 12, 14)
+control <- gam.control(epsilon = 0.00000001, bf.epsilon = 0.00000001)
+modelsGAM <- sapply(dfValues, function(dfVal) {
+  total.df <- dfVal * 13
+  fit <- gam(all_tot ~ pm10_s1 + meantemp + s(date, total.df), data = busan, family = poisson, control = control)
+  gamex <- gam.exact(fit)
+  gamex.coef <- gamex$coefficients["pm10_s1", "A-exact SE"]
+  c(coef(fit)["pm10_s1"], gamex.coef)
+})
+rng <- range(modelsGAM[1, ] - 1.96 * modelsGAM[2,], modelsGAM[1, ] + 1.96 * modelsGAM[2, ], 0)
+par(mar = c(4, 5, 2, 2),mfrow=c(2,1))
+plot(dfValues, modelsGAM[1, ], type = "b",pch = 20, ylim = rng, xlab = "df per year",ylab = expression(hat(beta)),main="(a)")
+lines(dfValues, modelsGAM[1, ] + 1.96 * modelsGAM[2,], lty = 2)
+lines(dfValues, modelsGAM[1, ] - 1.96 * modelsGAM[2, ], lty = 2)
+abline(h = 0, lty = 3)
+
+modelsGLM <- sapply(dfValues, function(dfVal) {
+  total.df <- dfVal * 14
+  fit <- glm(all_tot ~ pm10_s1 + meantemp + ns(date, total.df), data = busan,family = poisson)
+  summ <- summary(fit)
+  summ.coef <- summ$coefficients["pm10_s1",2]
+  c(coef(fit)["pm10_s1"], summ.coef)
+})
+rng <- range(modelsGLM[1, ] - 1.96 * modelsGLM[2,], modelsGLM[1, ] + 1.96 * modelsGLM[2, ], 0)
+plot(dfValues, modelsGLM[1, ], type = "b",pch = 20, ylim = rng, xlab = "df per year",ylab = expression(hat(beta)),main="(b)")
+lines(dfValues, modelsGLM[1, ] + 1.96 * modelsGLM[2,], lty = 2)
+lines(dfValues, modelsGLM[1, ] - 1.96 * modelsGLM[2, ], lty = 2)
+abline(h = 0, lty = 3)
+
+
+#교과서 예제를 부산에 적용한 것 (참고)
+library(mda)
+pm10 <- busan$pm10_s1
+x <- unclass(busan$date)
+use <- complete.cases(pm10, x)
+br.fit <- bruto(x[use], pm10[use])
+optimal.df <- br.fit$df
+library(gam)
+fit <- gam(all_tot ~ pm10_s1 + s(date,optimal.df), data = busan, family = quasipoisson)
+v <- gam.exact(fit)
+print(v$coefficients["pm10_s1", "Estimate"])
+print(v$coefficients["pm10_s1", "A-exact SE"])
+summary(fit)$dispersion
+
+
+use <- complete.cases(pm10, x)
+br.fit <- bruto(x[use], pm10[use])
+df.pm10 <- br.fit$df
+
+death <- busan$all_tot
+use <- complete.cases(death, x)
+br.fit <- bruto(x[use], death[use])
+df.death <- br.fit$df
+
+fit1 <- gam(all_tot ~ pm10_s1 + s(date,df.pm10), data = busan, family = quasipoisson)
+fit2 <- gam(all_tot ~ pm10_s1 + s(date,df.death), data = busan, family = quasipoisson)
+v1 <- gam.exact(fit1)
+v2 <- gam.exact(fit2)
+
+#보다 모던한 방법
+seoul.sub<-subset(seoul,year<2011)
+library(mgcv)
+fit3<-gam(all_tot~pm10+meanhumi+s(meantemp)+dow+s(ddd,k=4*5), data=seoul.sub, family=poisson)
+summary(fit3)
+plot(fit3)
+
+#여러 결과의 합성
+daegu<-subset(cities,sido==27)
+incheon<-subset(cities,sido==28)
+gwangju<-subset(cities,sido==29)
+daejeon<-subset(cities,sido==30)
+ulsan<-subset(cities,sido==31)
+
+library(mgcv)
+seoul.f<-gam(all_tot~pm10+meanhumi+s(meantemp)+dow+s(ddd,k=4*13),family=poisson(),data=seoul)
+se<-summary(seoul.f)
+busan.f<-gam(all_tot~pm10+meanhumi+s(meantemp)+dow+s(ddd,k=4*13),family=poisson(),data=busan)
+bu<-summary(busan.f)
+daegu.f<-gam(all_tot~pm10+meanhumi+s(meantemp)+dow+s(ddd,k=4*13),family=poisson(),data=daegu)
+dg<-summary(daegu.f)
+incheon.f<-gam(all_tot~pm10+meanhumi+s(meantemp)+dow+s(ddd,k=4*13),family=poisson(),data=incheon)
+ic<-summary(incheon.f)
+gwangju.f<-gam(all_tot~pm10+meanhumi+s(meantemp)+dow+s(ddd,k=4*13),family=poisson(),data=gwangju)
+gj<-summary(gwangju.f)
+daejeon.f<-gam(all_tot~pm10+meanhumi+s(meantemp)+dow+s(ddd,k=4*13),family=poisson(),data=daejeon)
+dj<-summary(daejeon.f)
+ulsan.f<-gam(all_tot~pm10+meanhumi+s(meantemp)+dow+s(ddd,k=4*13),family=poisson(),data=ulsan)
+us<-summary(ulsan.f)
+
+
+beta<-c(seoul.f$coefficients["pm10"],busan.f$coefficients["pm10"],daegu.f$coefficients["pm10"],incheon.f$coefficients["pm10"],daejeon.f$coefficients["pm10"],gwangju.f$coefficients["pm10"],ulsan.f$coefficients["pm10"])
+se<-c(se$se["pm10"], bu$se["pm10"],dg$se["pm10"],ic$se["pm10"],gj$se["pm10"],dj$se["pm10"],us$se["pm10"])
+n <- length(beta)
+rng <- range(beta - 1.96 * se, beta +1.96 * se)
+plot(beta, seq_len(n), xlim = rng, pch = 20,xlab = expression(hat(beta)), ylab = "City")
+segments(beta - 1.96 * se, seq(n), beta +1.96 * se, seq(n))
+abline(v = 0, lty = 2)
+
+library(metafor)
+meta1=rma(yi=beta, sei=se)
+summary(meta1)
